2f29237280fc7fd8d1c5251fce9c215eea7fdbd9
txtchirag <txtchirag@yahoo.com>
2020-04-20 00:43:25 +1000

    Update submission.py

--- submission.py
+++ submission.py
@@ -1,38 +1,42 @@
 def KMeans(data,centroid,max_iter):
     # Number of clusters
-    K = 256
+    K = centroid.shape[0]
+    
     thresh=1e-5
+    
     # Number of training data
     N = data.shape[0]
-    # Number of features in the data
-    M = data.shape[1]
 
 
     centroid_prev = np.zeros(centroid.shape) # to store previous centroids
     centroid_curr = np.array(centroid,copy=True) # Store current centroids
 
-    clusters = np.zeros(N)
-    prev_cluster=np.array(clusters,copy=True)
+    clusters = np.zeros(N,dtype='uint8')
     distances = np.zeros((N,K))
 
-    error = thresh +1
+    error = thresh+1
+    
     iter=0
 
-    while (error >thresh or clusters.all()!=prev_cluster.all()) and iter<max_iter:
+    while error >thresh and iter<max_iter:
         iter+=1
         # Measure the distance to every centroid
         for i in range(K):
             distances[:,i] = np.linalg.norm(data - centroid_curr[i], axis=1,ord=1)
         # Assign all training data to closest centroid
-        prev_cluster=np.array(clusters,copy=True)
         clusters = np.argmin(distances, axis = 1)
         
         centroid_prev = np.array(centroid_curr,copy=True)
         # Calculate median for every cluster and update the centroid
         for i in range(K):
-            centroid_curr[i] = np.median(data[clusters == i], axis=0)
+            centroid_curr[i] = np.nanmedian(data[clusters == i], axis=0)
         
         error = np.linalg.norm(centroid_curr - centroid_prev,ord=1)
+
+    for i in range(K):
+            distances[:,i] = np.linalg.norm(data - centroid_curr[i], axis=1,ord=1)
+    # Assign all training data to closest centroid
+    clusters = np.argmin(distances, axis = 1)
     
     return np.asarray(centroid_curr),np.asarray(clusters,dtype='uint8')
   
